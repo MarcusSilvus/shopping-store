40cbe22d8a9f7f03bd281f1d18662ba6764af1a4
Mark Silva <mark.allen.silva@gmail.com>
2022-11-01 16:22:38 -0700

    adding api

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import NavBar from './NavBar/NavBar'
 import ProductCard from './ProductCard/ProductCard'
 import SearchBar from './SearchBar/SearchBar'
 
 const App = () => {
+
+  const [productState, setProductState] = useState([])
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res=>res.json())
+      .then((productsArray) => {
+        const newProductState = productsArray.map((product) => {
+          return product
+        })
+        setProductState(newProductState)
+      })
+  }, [])
 
   return (
     <div>
--- src/Components/ProductCard/ProductCard.js
+++ src/Components/ProductCard/ProductCard.js
@@ -4,7 +4,7 @@ import './ProductCard.css'
 function ProductCard() {
   return (
     <div className='product-card'>
-      <h3>Title</h3>
+      <h3 className='title'>Title</h3>
       <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="men"></img>
       <h4>$</h4>
       <p>Product Description:</p>
